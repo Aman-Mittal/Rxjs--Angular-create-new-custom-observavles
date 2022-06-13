3d8d8693577b8dcc7f1691716fcb4002cc280736
Aman-Mittal <aman.mittal3052000@gmail.com>
2022-06-13 15:08:49 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// from converts almost any type into observables
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    const newObservable = new Observable<number>((observer) => {
+      for (let i = 0; i < 5; i++) {
+        setTimeout(() => {
+          observer.next(i);
+        }, 1000);
+      }
+    });
+
+    let observer = {
+      next: (data: number) => console.log(data),
+      error: (error: string) => console.log(error),
+      complete: () => console.log('The new observable is completed its task'),
+    };
+    newObservable.subscribe(observer);
+  }
+}
